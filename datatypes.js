9a13bd51bb0ca4e9790c9b32302159477adb0707
Sugan031 <suganthanv031@gmail.com>
2024-01-29 12:31:26 +0530

    started js

---
+++ datatypes.js
@@ -0,0 +1,100 @@
+var string = "apple"; //using double quotes
+var string2 = 'banana'; //using single quotes
+var string3 = 'this is my "page"';
+var string4 = `I like ${string}`; // using backticks or template string
+console.log(string);
+console.log(string2);
+console.log(string3);
+console.log(string4);
+
+console.log(typeof string);
+
+
+//Number datatype
+
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+
+let number1 = 340;
+let number2 = 500;
+
+let float1 = 0.3;
+let float2 = 0.2;
+
+// console.log(`Float value is ${float1* float2}`); // float value when use in arithmetic function the value will not be accurate
+console.log(`Float value is ${float1 + float2}`);
+console.log(number1);
+console.log(number2);
+console.log(number1+number2);
+
+let number3 = 9007199254740991;
+console.log(number3);
+console.log(typeof number3);
+
+let number4 =67n;
+console.log(number4);
+console.log(typeof number4);
+
+let y = 123e-5;
+console.log(y);
+
+let x = BigInt("123456789012345678901234567890");
+console.log(x);
+
+
+//Arithmetic operations with string and number
+console.log("*********************************************");
+
+console.log("10"+"10");//1010
+console.log("10"+10);//1010
+console.log(10+"10");//1010
+console.log("10"-2);//8
+console.log(10-"2");//8
+console.log("10"-"2");//8
+console.log("10"*"2");//20
+console.log("10"*2);//20
+console.log(10*"2");//20
+console.log("10"/"2");//5
+console.log("10"/2);//5
+console.log(10/"2");//5
+
+console.log("10"/"apple");//NaN
+console.log("10"*"apple");//NaN
+console.log("10"-"apple");//NaN
+
+console.log(45-NaN);//NaN
+
+
+
+console.log("*********************************************");
+// boolean datatype
+
+let bool1 = 5;
+let bool2 = 5;
+let bool3 =6;
+let name1;
+
+console.log((bool1==bool2));//false
+console.log((bool2==bool3));//false
+console.log(Boolean(name1)); //false
+
+console.log(Boolean(1)); //true
+console.log(Boolean(-1)); //true
+console.log(Boolean(0)); //false
+console.log(Boolean(NaN)); //false
+console.log(Boolean("a")); //false
+console.log(Boolean("")); //false
+console.log(Boolean(undefined));
+
+//undefined
+console.log(name1);//undefined
+// let myname = ""; // type of string
+let myname = undefined;
+console.log(myname);
+console.log(typeof myname);
+
+//null
+
+let value = null;
+console.log(value);
+console.log(typeof value);
---
+++ index.js
@@ -0,0 +1,38 @@
+console.log("Hello world!");
+
+var x =10;
+var y = 20;
+ var x =5;
+
+ var b ;
+ b=1;
+ var b =2;
+ 
+
+
+const firstName = "John";
+const lastName = "Doe";
+
+let price_1 = 300;
+let price_2 = 400;
+
+let a;
+a=20;
+a=30;
+{
+    let a =45;
+    console.log(a);
+}
+console.log(a);
+
+const d = 1;
+{
+    const d =23;
+    console.log(d);
+}
+
+let total = price_1 + price_2;
+
+console.log(x*y);
+console.log(firstName +" "+ lastName);
+console.log(total);
